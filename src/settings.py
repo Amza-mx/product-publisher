f6d362706c10e09a81c3f0377f10549de943234d
Leonardo Amaya <jose.amaya@yuhu.mx>
2025-02-26 04:59:37 -0600

    settings: WooCommerce API configuration added

--- src/settings.py
+++ src/settings.py
@@ -3,6 +3,11 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
+# WooCommerce API
+WOOCOMMERCE_URL = os.getenv("WOOCOMMERCE_URL", "default")
+WOOCOMMERCE_CONSUMER_KEY = os.getenv("WOOCOMMERCE_CONSUMER_KEY", "default")
+WOOCOMMERCE_CONSUMER_SECRET = os.getenv("WOOCOMMERCE_CONSUMER_SECRET", "default")
+
 # Keepa API
 KEEPA_API_KEY = os.getenv("KEEPA_API_KEY", "default")
 KEEPA_CATEGORY_ID = os.getenv("KEEPA_CATEGORY_ID", 0)
